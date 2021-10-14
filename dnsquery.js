7a6082b5b96aa32c2f9309fde3869f563a134d1f
xk <jorge@jorgechamorro.com>
2021-10-14 22:42:55 +0200

    No funciona(ban) los overrides de msg.payload.
    Y más cosas.

--- dnsquery.js
+++ dnsquery.js
@@ -1,103 +1,54 @@
 //2020-07-25 dnsquery jorge@jorgechamorro.com
+//2021-10-14 v1.0.7
 
 module.exports= function(RED) {
 
     "use strict";
     var dns= require("dns");
-    var initial_dns_ip= dns.getServers()[0];
 
     function dnsquery (config) {
 
         RED.nodes.createNode(this, config);
         var node= this;
-        var now_using_dns_ip= dns.getServers()[0];
 
-        node.domain_name = config.domain_name || ""
-        node.domain_nameType = config.domain_nameType || "str";
+        node.domain_name = config.domain_name || "";
         node.record_type = config.record_type || "A";
-        node.record_typeType = config.record_typeType || "rectype";
-        node.dns_ip = config.dns_ip;
-
-        function validar_ip (ip) {
-          var r= ip.split(".");
-          if (r.length !== 4) return "";
-          r= r.filter(function(v,i,o) {
-            var n= Math.floor(+v);
-            if (v !== (""+n)) return false;
-            if ((n<0) || (n>255)) return false;
-            return true;
-          });
-          return (r.length === 4) ? r.join(".") : "";
-        }
-
-        function isArray (p) {
-          //Miller device
-          return (({}).toString.apply(p).toLowerCase().indexOf("array") > 0);
-        }
-
+        node.dns_ip = config.dns_ip || dns.getServers()[0];
+        
         node.on('input', function (msg) {
 
-            function cb (err, o) {
+            var payload = msg.payload;
+            var dns_ip = node.dns_ip;
+            var domain = node.domain_name;
+            var record_type = node.record_type;
+
+            //msg.payload overrides?
+            //Si msg.payload contiene domain_name se usa msg.payload.domain_name
+            //Si no, se usa config.domain_name
+            //Idem para record_type
+            //Idem para dns_ip
+            
+            if (typeof payload === "object") {
+                if (payload.dns_ip) dns_ip = payload.dns_ip;
+                if (payload.domain_name) domain = payload.domain_name;
+                if (payload.record_type) record_type = payload.record_type;
+            }
+            
+            if (dns_ip !== dns.getServers()[0]) dns.setServers([ dns_ip ]);
+            
+            dns.resolve(domain, record_type, function cb (err, o) {
 
               msg.payload= {
                 ok:(!err),
-                domain:domain,
-                type:record_type,
-                answer:o,
                 error:err,
-                dns_ip: now_using_dns_ip
+                dns_ip: dns_ip,
+                type:record_type,
+                domain:domain,
+                answer:o
               };
 
               node.send(msg);
-
-            };
-
-
-            var err;
-            var domain;
-            var record_type;
-
-            //Qué domain name hemos de mirar?
-            //Si msg.payload contiene domain_name se usa msg.payload.domain_name
-            //Si no, se intenta usar config.domain_name
-            
-            RED.util.evaluateNodeProperty(node.domain_name,node.domain_nameType,node,msg,(error,value) => {
-              if (error) {
-                  err = error;
-              } else {
-                domain = value
-              }
-            }); 
-            RED.util.evaluateNodeProperty(node.record_type,node.record_typeType,node,msg,(error,value) => {
-              if (error) {
-                  err = error;
-              } else {
-                record_type = value || "A"
-              }
-            }); 
-
-            if (err) {
-              setTimeout(function () { cb(err, []); }, 0);
-              return;
-            }
-
-            if (node.dns_ip) {
-              if (typeof node.dns_ip === "string") {
-                var dns_ip= validar_ip(node.dns_ip);
-                if (dns_ip) {
-                  if (dns_ip !== now_using_dns_ip) {
-                    dns.setServers([ dns_ip ]);
-                    now_using_dns_ip= dns_ip;
-                  }
-                }
-              }
-            }
-            else if (now_using_dns_ip !== initial_dns_ip) {
-              dns.setServers([ initial_dns_ip ]);
-              now_using_dns_ip= initial_dns_ip;
-            }
-
-            dns.resolve(domain, record_type, cb);
+            });
         });
     }
     RED.nodes.registerType("dnsquery", dnsquery);
